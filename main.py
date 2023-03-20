3f99a527457af51892808ba925cff713d4d36719
soyflacko <126542076+lottaruntz@users.noreply.github.com>
2023-03-20 07:06:47 +0000

    A3-2

--- main.py
+++ main.py
@@ -2,7 +2,24 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    email = input('Enter your email: ')
+
+    if(email[0].isalpha):
+        if(len(email) > 5 and len(email) < 30):
+            if(email.find('@')):
+                if('.' in email):
+                    print('True')
+                else:
+                    print('False')
+            else:
+                print('False')
+        else:
+            print('False')
+    else:
+        print('False')
     pass
+
+
 
 
 if __name__ == '__main__':
